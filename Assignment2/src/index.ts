edc7534ac9a26e94ca2306ab8a126229a3823919
irina-grabhermeier <igr7222@students.fhv.at>
2021-03-10 11:44:17 +0100

    index.html is now getting src from bundle.js

--- Assignment2/src/index.ts
+++ Assignment2/src/index.ts
@@ -1,8 +1,6 @@
 import square from './square';
 import sum from './sum';
 
-
-//const sumi = sum('5', 8);
-
 document.writeln('The sum of 5 and 8 is ', sum(5, 8).toString(), '<br>');
 document.writeln('The square of 6 is ', square(6).toString(), '<br>');
+
