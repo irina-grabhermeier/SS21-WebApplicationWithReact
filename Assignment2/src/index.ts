d796a97f9cd8c882f8a5af79fcf21b846b8e3ab0
Irina <igr7222@students.fhv.at>
2021-03-24 11:34:34 +0100

    merge stuff

--- Assignment2/src/index.ts
+++ Assignment2/src/index.ts
@@ -1,6 +1,8 @@
 import square from './square';
 import sum from './sum';
 
+const sumCausesError = sum('2', 1);
+
 document.writeln('The sum of 5 and 8 is ', sum(5, 8).toString(), '<br>');
 document.writeln('The square of 6 is ', square(6).toString(), '<br>');
 
