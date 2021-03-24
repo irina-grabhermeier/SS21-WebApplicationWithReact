b4fb31fa358ca6dd14fd56779be210290631cfa2
Irina <igr7222@students.fhv.at>
2021-03-24 19:11:14 +0100

    assignment 3 - added some navigation features and message class and form

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -15,23 +15,64 @@ function onMessagesContainerClick() {
 
 }
 
+const allMessages = [];
 
 
+const addMessageForm = document.getElementById('messageForm');
 
 
+addMessageForm.addEventListener('submit', function (event) {
+    const formData = new FormData(addMessageForm);
 
-/*
-const form = document.getElementById('form');
+    const headerInput = formData.get('headerInput');
+    const bodyInput = formData.get('bodyInput');
 
-form.addEventListener('submit', function(event) {
+    if ((headerInput !== '' && headerInput !== undefined) || (bodyInput !== '' && bodyInput !== undefined)) {
 
-    const formData = new FormData(form);
+        const message = new Message(headerInput, bodyInput, true);
 
-    const firstname = formData.get('firstName');
-    const lastname = formData.get('lastName');
+        allMessages.push(message);
+        console.log(message);
 
-    console.log($"Firstname ")
+        addMessageForm.reset();
 
-    form.reset();
+    }
 });
-*/
+
+function messagesLoaded() {
+    const listOfMessages = document.getElementById('listOfMessages');
+
+
+    unreadMessages.forEach(m => {
+
+    });
+
+    const headerText = document.createElement('p');
+    const bodyText = document.createElement('p');
+
+    headerText.innerText = message.headerInput;
+    bodyText.innerText = message.bodyInput;
+
+    headerText.style.fontWeight = 'bold';
+
+    const listItem = document.createElement('li');
+    listItem.appendChild(headerText);
+    listItem.appendChild(bodyText);
+
+    listItem.style.backgroundColor = 'light-blue';
+    listItem.classList.add('list-group-item');
+
+    console.log(message);
+
+    listOfMessages.appendChild(listItem);
+}
+
+class Message {
+
+    constructor(headerText, bodyText, isUnread) {
+        this.headerText = headerText;
+        this.bodyText = bodyText;
+        this.isUnread = isUnread;
+    }
+
+}
