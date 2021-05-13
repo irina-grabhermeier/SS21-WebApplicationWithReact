15cf559364b4a345e9b059f6996b16966ba2e35e
irina-grabhermeier <igr7222@students.fhv.at>
2021-05-13 17:37:28 +0200

    fixed DataPresenter tests

--- Assignment10/assignment10/src/tests/DataPresenter.test.tsx
+++ Assignment10/assignment10/src/tests/DataPresenter.test.tsx
@@ -1,11 +1,20 @@
 import { shallow } from "enzyme";
 import DataPresenter from "../components/DataPresenter";
 
-test('Data is correct', () => {
+test('Correct data length', () => {
     const testData = ['Data 0', 'Data 1', 'Data 2', 'Data 3'];
     const wrapper = shallow(<DataPresenter data={testData} />);
 
     expect(wrapper.find('ul').children()).toHaveLength(testData.length);
+});
 
 
+test('Correct data', () => {
+    const testData = ['Data 0', 'Data 1', 'Data 2', 'Data 3'];
+    const wrapper = shallow(<DataPresenter data={testData} />);
+
+    for (let i = 0; i < testData.length; i++) {
+        const liWrapper = wrapper.find('ListItem');
+        expect(wrapper.find('ul').children().contains('Data ' + i)).toBe(true);
+    }
 });
