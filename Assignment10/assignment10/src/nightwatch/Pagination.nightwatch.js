a1cb9a40ad511046dc9a8f95651a37bc12c0d65c
irina-grabhermeier <igr7222@students.fhv.at>
2021-05-13 17:53:10 +0200

    refactored nightwatch tests

--- Assignment10/assignment10/src/nightwatch/Pagination.nightwatch.js
+++ Assignment10/assignment10/src/nightwatch/Pagination.nightwatch.js
@@ -1,14 +1,4 @@
 module.exports = {
-    'Test overall website': function (browser) {
-        browser
-            .url('http://localhost:3000')
-            .waitForElementVisible('body')
-            .assert.visible('#previous')
-            .assert.visible('#current')
-            .assert.visible('#next')
-            .assert.containsText('#current', '1 / 10')
-            .end();
-    },
     'Test next button': function (browser) {
         browser
             .url('http://localhost:3000')
@@ -19,23 +9,36 @@ module.exports = {
             .assert.containsText('#current', '2 / 10')
             .end();
     },
-    'Test next button on last page': function (browser) {
+    'Test next button on last page disabled': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
             .assert.visible('#next')
             .assert.containsText('#current', '1 / 10')
-            .click('#next') // Page 2
-            .click('#next') // Page 3
-            .click('#next') // Page 4
-            .click('#next') // Page 5
-            .click('#next') // Page 6
-            .click('#next') // Page 7
-            .click('#next') // Page 8
-            .click('#next') // Page 9
-            .click('#next') // Page 10
+            .click('#next') // On page 2
+            .click('#next') // On page 3
+            .click('#next') // On page 4
+            .click('#next') // On page 5
+            .click('#next') // On page 6
+            .click('#next') // On page 7
+            .click('#next') // On page 8
+            .click('#next') // On page 9
+            .click('#next') // On page 10
+            .assert.containsText('#current', '10 / 10')
+            .click('#next')
             .assert.containsText('#current', '10 / 10')
             .assert.attributeEquals('#next', 'disabled', 'true')
+            .end();
+    },
+    'Test previous button on first page disabled': function (browser) {
+        browser
+            .url('http://localhost:3000')
+            .waitForElementVisible('body')
+            .assert.visible('#previous')
+            .assert.containsText('#current', '1 / 10')
+            .click('#previous')
+            .assert.containsText('#current', '1 / 10')
+            .assert.attributeEquals('#previous', 'disabled', 'true')
             .end();
     },
     'Test previous button': function (browser) {
@@ -44,22 +47,10 @@ module.exports = {
             .waitForElementVisible('body')
             .assert.visible('#previous')
             .assert.visible('#next')
-            .assert.containsText('#current', '1 / 10')
             .click('#next')
+            .assert.containsText('#current', '2 / 10')
             .click('#previous')
             .assert.containsText('#current', '1 / 10')
-            .assert.attributeEquals('#previous', 'disabled', 'true')
-            .end();
-    },
-    'Test previous button on first page': function (browser) {
-        browser
-            .url('http://localhost:3000')
-            .waitForElementVisible('body')
-            .assert.visible('#previous')
-            .assert.containsText('#current', '1 / 10')
-            .click('#previous')
-            .assert.containsText('#current', '1 / 10')
-            .assert.attributeEquals('#previous', 'disabled', 'true')
             .end();
     },
 };
