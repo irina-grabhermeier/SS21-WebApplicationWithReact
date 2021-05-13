7aa7c19e5f7dcc4f55c5cc873ae2ca76ce648393
irina-grabhermeier <igr7222@students.fhv.at>
2021-05-13 17:47:11 +0200

    changed nightwatch tests

--- Assignment10/assignment10/src/nightwatch/Pagination.nightwatch.js
+++ Assignment10/assignment10/src/nightwatch/Pagination.nightwatch.js
@@ -1,31 +1,30 @@
 module.exports = {
-    'Test overall website' : function (browser) {
+    'Test overall website': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
-            .assert.titleContains('Testing | dlu9576')
-            .assert.visible('#prev')
-            .assert.visible('#current-page')
+            .assert.visible('#previous')
+            .assert.visible('#current')
             .assert.visible('#next')
-            .assert.containsText('#current-page', '1 / 10')
+            .assert.containsText('#current', '1 / 10')
             .end();
     },
-    'Test next button' : function (browser) {
+    'Test next button': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
             .assert.visible('#next')
-            .assert.containsText('#current-page', '1 / 10')
+            .assert.containsText('#current', '1 / 10')
             .click('#next')
-            .assert.containsText('#current-page', '2 / 10')
+            .assert.containsText('#current', '2 / 10')
             .end();
     },
-    'Test next button on last page' : function (browser) {
+    'Test next button on last page': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
             .assert.visible('#next')
-            .assert.containsText('#current-page', '1 / 10')
+            .assert.containsText('#current', '1 / 10')
             .click('#next') // Page 2
             .click('#next') // Page 3
             .click('#next') // Page 4
@@ -35,32 +34,32 @@ module.exports = {
             .click('#next') // Page 8
             .click('#next') // Page 9
             .click('#next') // Page 10
-            .assert.containsText('#current-page', '10 / 10')
+            .assert.containsText('#current', '10 / 10')
             .assert.attributeEquals('#next', 'disabled', 'true')
             .end();
     },
-    'Test previous button' : function (browser) {
+    'Test previous button': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
-            .assert.visible('#prev')
+            .assert.visible('#previous')
             .assert.visible('#next')
-            .assert.containsText('#current-page', '1 / 10')
+            .assert.containsText('#current', '1 / 10')
             .click('#next')
-            .click('#prev')
-            .assert.containsText('#current-page', '1 / 10')
-            .assert.attributeEquals('#prev', 'disabled', 'true')
+            .click('#previous')
+            .assert.containsText('#current', '1 / 10')
+            .assert.attributeEquals('#previous', 'disabled', 'true')
             .end();
     },
-    'Test previous button on first page' : function (browser) {
+    'Test previous button on first page': function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body')
-            .assert.visible('#prev')
-            .assert.containsText('#current-page', '1 / 10')
-            .click('#prev')
-            .assert.containsText('#current-page', '1 / 10')
-            .assert.attributeEquals('#prev', 'disabled', 'true')
+            .assert.visible('#previous')
+            .assert.containsText('#current', '1 / 10')
+            .click('#previous')
+            .assert.containsText('#current', '1 / 10')
+            .assert.attributeEquals('#previous', 'disabled', 'true')
             .end();
     },
 };
